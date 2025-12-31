9e56ab8d29d25446f3bd5cf9ed1dac0879c7cbd6
Harry Dhillon <hdhillon478@gmail.com>
2025-12-31 15:39:17 +0000

    feat: pass isVisualizationActiveRef to Grid component in GridContainer

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,9 +1,12 @@
+import { useRef } from "react";
 import Grid from "./Grid";
 
 export default function GridContainer() {
+  const isVisualizationActiveRef = useRef(false)
+
   return (
     <div className='h-screen w-screen flex flex-col'>
-      <Grid />
+      <Grid isVisualizationActiveRef={isVisualizationActiveRef} />
     </div>
   );
 }
