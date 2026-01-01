a9d7e3ca70a0686e1c96c6df5c1cccb6a0bafa15
Harry Dhillon <hdhillon478@gmail.com>
2026-01-01 13:17:58 +0000

    fix:simplify return fragment in GridContainer

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -2,11 +2,11 @@ import { useRef } from "react";
 import Grid from "./Grid";
 
 export default function GridContainer() {
-  const isVisualizationActiveRef = useRef(false)
+  const isVisualizationActiveRef = useRef(false);
 
   return (
-    <div className='h-screen w-screen flex flex-col'>
+    <>
       <Grid isVisualizationActiveRef={isVisualizationActiveRef} />
-    </div>
+    </>
   );
 }
