9c363d1527859818a0f1c538d427ca1e8c9d1533
Harry Dhillon <hdhillon478@gmail.com>
2026-01-01 17:38:13 +0000

    feat: add runMazeAlgorithm function with type definitions for maze execution

---
+++ src/lib/runMazeAlgorithm.ts
@@ -0,0 +1,19 @@
+import type { Grid, Maze, Speed, Tile } from "./types";
+
+type runMazeAlgorithmProps = {
+  maze: Maze;
+  grid: Grid;
+  startTile: Tile;
+  endTile: Tile;
+  setIsDisabled: (disabled: boolean) => void;
+  speed: Speed;
+};
+
+export async function runMazeAlgorithm({
+  maze,
+  grid,
+  startTile,
+  endTile,
+  setIsDisabled,
+  speed,
+}: runMazeAlgorithmProps) {}
