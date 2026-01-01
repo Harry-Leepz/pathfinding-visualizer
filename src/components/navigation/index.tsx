934e828e73d631f301cddd3735540cbde27df5eb
Harry Dhillon <hdhillon478@gmail.com>
2026-01-01 13:18:04 +0000

    feat: add Navigation component for header display

---
+++ src/components/navigation/index.tsx
@@ -0,0 +1,11 @@
+export default function Navigation() {
+  return (
+    <div className='flex items-center justify-center min-h-18 border-b shadow-slate-600 sm:px-5 px-0'>
+      <div className='flex items-center lg:justify-between justify-center w-full sm:w-208'>
+        <h1 className='lg:flex hidden w-[40%] text-2xl pl-1'>
+          Pathfinding Visualizer
+        </h1>
+      </div>
+    </div>
+  );
+}
