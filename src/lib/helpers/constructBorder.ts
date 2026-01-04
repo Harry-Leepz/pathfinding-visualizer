10b2222f20e9f962e82c5e717de8f7c5fb05acfa
Harry Dhillon <hdhillon478@gmail.com>
2026-01-04 18:37:46 +0000

    feat: implement constructBorder function for grid visualization

---
+++ src/lib/helpers/constructBorder.ts
@@ -0,0 +1,55 @@
+import { BASE_TILE_STYLE, MAX_COLS, MAX_ROWS, SLEEP_TIME } from "../constants";
+import { isEqualTiles } from "../helpers";
+import type { Grid, Tile } from "../types";
+import sleep from "./sleep";
+
+export default async function constructBorder(
+  grid: Grid,
+  startTile: Tile,
+  endTile: Tile
+) {
+  const shape = [
+    { row: 0, col: 1 }, // right
+    { row: 0, col: -1 }, // left
+    { row: 1, col: 0 }, // down
+    { row: -1, col: 0 }, // up
+  ];
+
+  let row = 0;
+  let col = 0;
+
+  for (let i = 0; i < 4; i++) {
+    const directtion = shape[i];
+
+    while (
+      row + directtion.row >= 0 &&
+      row + directtion.row < MAX_ROWS &&
+      col + directtion.col >= 0 &&
+      col + directtion.col < MAX_COLS
+    ) {
+      row += directtion.row;
+      col += directtion.col;
+
+      if (
+        !isEqualTiles(grid[row][col], startTile) &&
+        !isEqualTiles(grid[row][col], endTile)
+      ) {
+        const tileElement = document.getElementById(`${row}-${col}`);
+        if (tileElement) {
+          tileElement.classList.add(
+            ...BASE_TILE_STYLE.split(" "),
+            "animate-wall"
+          );
+        }
+
+        await sleep(SLEEP_TIME);
+      }
+    }
+  }
+
+  if (  row < 0 ) row = 0;
+  if ( row >= MAX_ROWS) row = MAX_ROWS - 1;
+  if ( col < 0 ) col = 0;
+  if ( col >= MAX_COLS) col = MAX_COLS - 1;
+}
+ 
