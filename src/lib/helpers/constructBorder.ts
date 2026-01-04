1560f3a31c14fb3e021efbe2d14b859fe14d8d7d
Harry Dhillon <hdhillon478@gmail.com>
2026-01-04 19:52:39 +0000

    feat: update constructBorder function to use WALL_TILE_STYLE and fix shape order

--- src/lib/helpers/constructBorder.ts
+++ src/lib/helpers/constructBorder.ts
@@ -1,4 +1,4 @@
-import { BASE_TILE_STYLE, MAX_COLS, MAX_ROWS, SLEEP_TIME } from "../constants";
+import { MAX_COLS, MAX_ROWS, SLEEP_TIME, WALL_TILE_STYLE } from "../constants";
 import { isEqualTiles } from "../helpers";
 import type { Grid, Tile } from "../types";
 import sleep from "./sleep";
@@ -10,8 +10,8 @@ export default async function constructBorder(
 ) {
   const shape = [
     { row: 0, col: 1 }, // right
-    { row: 0, col: -1 }, // left
     { row: 1, col: 0 }, // down
+    { row: 0, col: -1 }, // left
     { row: -1, col: 0 }, // up
   ];
 
@@ -37,7 +37,7 @@ export default async function constructBorder(
         const tileElement = document.getElementById(`${row}-${col}`);
         if (tileElement) {
           tileElement.classList.add(
-            ...BASE_TILE_STYLE.split(" "),
+            ...WALL_TILE_STYLE.split(" "),
             "animate-wall"
           );
         }
@@ -47,9 +47,8 @@ export default async function constructBorder(
     }
   }
 
-  if (  row < 0 ) row = 0;
-  if ( row >= MAX_ROWS) row = MAX_ROWS - 1;
-  if ( col < 0 ) col = 0;
-  if ( col >= MAX_COLS) col = MAX_COLS - 1;
+  if (row < 0) row = 0;
+  if (row >= MAX_ROWS) row = MAX_ROWS - 1;
+  if (col < 0) col = 0;
+  if (col >= MAX_COLS) col = MAX_COLS - 1;
 }
- 
